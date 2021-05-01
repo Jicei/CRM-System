b250936a74ba8e4a098461cd1c87e144ae260e8f
Jicei <31828588+Jicei@users.noreply.github.com>
2021-05-01 17:50:52 +0300

    Add Swagger

--- CRM System/Startup.cs
+++ CRM System/Startup.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,9 +24,30 @@ namespace CRM_System
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
-        }
 
+            services.AddCors(o => o.AddPolicy("CorsApi", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
+            services.AddMvc();
+            services.AddControllers();
+            services.AddControllersWithViews();
+
+            // services.AddDbContext<DevoxTestTaskDbContext>(options =>
+            //     options.UseSqlServer(Configuration.GetConnectionString("DevoxTestTaskDatabase")));
+
+            services.AddSwaggerGen(c =>
+             {
+                 c.SwaggerDoc(Configuration.GetSection("SwaggerOptions:Version").Value, new OpenApiInfo
+                 {
+                     Version = Configuration.GetSection("SwaggerOptions:Version").Value,
+                     Title = Configuration.GetSection("SwaggerOptions:Title").Value,
+                     Description = Configuration.GetSection("SwaggerOptions:Description").Value
+                 });
+             });
+        }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -36,6 +59,14 @@ namespace CRM_System
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(option =>
+            {
+                option.SwaggerEndpoint(Configuration.GetSection("SwaggerOptions:UIEndpoint").Value, Configuration.GetSection("SwaggerOptions:Description").Value);
+                option.RoutePrefix = string.Empty;
+            });
             app.UseStaticFiles();
 
             app.UseRouting();
