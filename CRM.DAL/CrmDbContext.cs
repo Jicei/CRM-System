df072851487b07cb21771eeb9f9b5a2667334abc
Jicei <31828588+Jicei@users.noreply.github.com>
2021-05-01 23:36:18 +0300

    Add DBConnection

---
+++ CRM.DAL/CrmDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRM.DAL
+{
+    public class CrmDbContext: DbContext
+    {
+        public CrmDbContext(DbContextOptions<CrmDbContext> options)
+            : base(options)
+        { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ CRM.WebApi/Startup.cs
@@ -0,0 +1,85 @@
+using CRM.DAL;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRM.WebAPi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddCors(o => o.AddPolicy("CorsApi", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
+            services.AddMvc();
+            services.AddControllers();
+            services.AddControllersWithViews();
+
+             services.AddDbContext<CrmDbContext>(options =>
+                 options.UseSqlServer(Configuration.GetConnectionString("devConnection")));
+
+            services.AddSwaggerGen(c =>
+             {
+                 c.SwaggerDoc(Configuration.GetSection("SwaggerOptions:Version").Value, new OpenApiInfo
+                 {
+                     Version = Configuration.GetSection("SwaggerOptions:Version").Value,
+                     Title = Configuration.GetSection("SwaggerOptions:Title").Value,
+                     Description = Configuration.GetSection("SwaggerOptions:Description").Value
+                 });
+             });
+        }
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(option =>
+            {
+                option.SwaggerEndpoint(Configuration.GetSection("SwaggerOptions:UIEndpoint").Value, Configuration.GetSection("SwaggerOptions:Description").Value);
+                option.RoutePrefix = string.Empty;
+            });
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseCors("CorsApi");
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
